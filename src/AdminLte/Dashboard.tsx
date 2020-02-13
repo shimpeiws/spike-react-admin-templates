1d7244ce9304ed134539a9d586cde24c00045159
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-02-14 04:36:31 +0900

    Introduce AdminLTE

---
+++ src/AdminLte/Dashboard.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const reactjsAdminlte = require("adminlte-reactjs");
+
+export default function Dashboard(_: RouteComponentProps) {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      {/* <reactjsAdminlte.InfoTile
+        width="3"
+        content=""
+        icon="fa-envelope-o"
+        stats="1,410"
+        subject="Messages"
+        theme="bg-aqua"
+      /> */}
+    </>
+  );
+}
