bfcb76cf3d069925fa9c03134e1470bfa79e5819
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-02-14 03:59:48 +0900

    Initial commit

---
+++ src/Index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Routes } from "./Routes";
+
+ReactDOM.render(
+  <>
+    <Routes />
+  </>,
+  document.getElementById("app")
+);
---
+++ src/Routes.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export class Routes extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <h1>spike-react-admin-templates</h1>
+      </div>
+    );
+  }
+}
